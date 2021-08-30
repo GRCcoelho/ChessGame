988285b4a45b1564f7a3e51360a32e02aa23debb
GRCcoelho <coelhogabriel2411@gmail.com>
2021-08-30 20:26:21 -0300

    Implementação do tabuleiro

---
+++ ChessGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using ChessGame.tabuleiro;
+using System;
+
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine(); 
+        }
+    }
+}
