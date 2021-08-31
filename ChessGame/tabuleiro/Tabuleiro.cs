f06105638fba1a7f7b5c58786f6909cfd695da6f
GRCcoelho <coelhogabriel2411@gmail.com>
2021-08-31 19:22:09 -0300

    Tratando erros com exceção personalizada

--- ChessGame/tabuleiro/Tabuleiro.cs
+++ ChessGame/tabuleiro/Tabuleiro.cs
@@ -29,8 +29,18 @@ namespace ChessGame.tabuleiro
             return pecas[pos.linha, pos.coluna];
         }
 
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
@@ -42,6 +52,14 @@ namespace ChessGame.tabuleiro
                 return false;
             }
             return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
